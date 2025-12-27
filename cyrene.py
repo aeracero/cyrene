314aa5f79f52124a7fca59d12414eb468eaf8e83
aeracero <99156748+aeracero@users.noreply.github.com>
2025-12-27 11:25:50 +0900

    Add files via upload

--- cyrene.py
+++ cyrene.py
@@ -1718,7 +1718,7 @@ async def on_message(message: discord.Message):
 
         unlocked_now = False
         wins = get_janken_wins(user_id)
-        if wins >= 30 and not is_nanoka_unlocked(user_id):
+        if wins >= 307 and not is_nanoka_unlocked(user_id):
             set_nanoka_unlocked(user_id, True)
             unlocked_now = True
 
